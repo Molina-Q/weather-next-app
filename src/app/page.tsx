4f5a4aec48096cd160112095172c2b575c6add6e
Molina-Q <quentinmolina1@gmail.com>
2024-03-24 15:24:30 +0100

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
-  return (<div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
-    <Navbar />
-  </div>
-    );
+  return (
+    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
+      <Navbar />
+    </div>
+  );
 }
