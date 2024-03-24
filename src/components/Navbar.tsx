e0e8d57f61625f4f6d67c15f8a4159014e4f9918
Molina-Q <quentinmolina1@gmail.com>
2024-03-24 19:52:39 +0100

    minor changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,15 +12,15 @@ export default function Navbar({}: Props) {
   return (
     <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
         <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
-            <p className='flex items-center justify-center gap-2'>
+            <div className='flex items-center justify-center gap-2'>
                 <h2 className='text-gray-500 text-3xl'>Weather</h2>
                 <MdWbSunny className='text-3xl mt-1 text-yellow-300'/>
-            </p>
+            </div>
             <section className="flex gap-2 items-center">
                 <MdMyLocation className="text-2xl  text-gray-400 hover:opacity-80 cursor-pointer" />
                 <MdOutlineLocationOn className='text-3xl' />
                 <p className='text-slate-900/80 text-sm'>France</p>
-                <div><SearchBox value={''} onSubmit={} onChange={} /></div>
+                {/* <div><SearchBox value={''} onSubmit={} onChange={} /></div> */}
             </section>
         </div>
     </nav>
