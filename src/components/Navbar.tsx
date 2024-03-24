36652d791260cb374c52a5afa28928fcc2efde3e
Molina-Q <quentinmolina1@gmail.com>
2024-03-24 15:24:51 +0100

    added icons and searchbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,28 @@
+/** @format */
+
 import React from 'react'
+import { MdWbSunny } from "react-icons/md";
+import { MdMyLocation } from "react-icons/md";
+import { MdOutlineLocationOn } from "react-icons/md";
+import SearchBox from './SearchBox';
 
 type Props = {}
 
 export default function Navbar({}: Props) {
   return (
     <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
-        <div className='h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto'>
-            <p className='flex items-center justify-center gap-2'></p>
-            <h2 className='text-gray-500 text-3x1'>Weather</h2>
+        <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
+            <p className='flex items-center justify-center gap-2'>
+                <h2 className='text-gray-500 text-3xl'>Weather</h2>
+                <MdWbSunny className='text-3xl mt-1 text-yellow-300'/>
+            </p>
+            <section className="flex gap-2 items-center">
+                <MdMyLocation className="text-2xl  text-gray-400 hover:opacity-80 cursor-pointer" />
+                <MdOutlineLocationOn className='text-3xl' />
+                <p className='text-slate-900/80 text-sm'>France</p>
+                <div><SearchBox value={''} onSubmit={} onChange={} /></div>
+            </section>
         </div>
-        Navbar
     </nav>
   )
 }
