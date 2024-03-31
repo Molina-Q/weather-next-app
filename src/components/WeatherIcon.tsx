a26234f5f7c450473471bb457686710318bacc54
Molina-Q <quentinmolina1@gmail.com>
2024-03-31 22:57:11 +0200

    minor changes

--- src/components/WeatherIcon.tsx
+++ src/components/WeatherIcon.tsx
@@ -1,19 +1,23 @@
-import { cn } from '@/utils/cn'
-import Image from 'next/image'
-import React from 'react'
+/** @format */
 
-type Props = {}
+import React from "react";
+import Image from "next/image";
+import { cn } from "@/utils/cn";
 
-export default function WeatherIcon(props: React.HTMLProps<HTMLDivElement> & {iconName:string}) {
+type Props = {};
+
+export default function WeatherIcon(
+  props: React.HTMLProps<HTMLDivElement> & { iconName: string }
+) {
   return (
-    <div {...props} className={cn('relative h-20 w-20')}>
-        <Image
-            width={100}
-            height={100}
-            alt="weather-icon"
-            className='absolute h-full w-full'
-            src={`https://openweathermap.org/img/wn/${props.iconName}@4x.png`} 
-        />
+    <div title={props.iconName} {...props} className={cn("relative h-20 w-20")}>
+      <Image
+        width={100}
+        height={100}
+        alt="weather-icon"
+        className="absolute h-full w-full"
+        src={`https://openweathermap.org/img/wn/${props.iconName}@4x.png`}
+      />
     </div>
-  )
+  );
 }
