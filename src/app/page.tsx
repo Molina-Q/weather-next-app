fa938169bee708def27023d9694ba0b757afbe6c
Molina-Q <quentinmolina1@gmail.com>
2024-03-31 23:05:03 +0200

    use function to change the icon to day or night

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Container from "@/components/Container";
 import Navbar from "@/components/Navbar";
 import WeatherIcon from "@/components/WeatherIcon";
 import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
+import { getDayOrNightIcon } from "@/utils/getDayOrNightIcon";
 import axios from "axios";
 import { format, parseISO } from "date-fns";
 import Image from "next/image";
@@ -133,7 +134,7 @@ export default function Home() {
                       <p className="whitespace-nowrap">
                         {format(parseISO(d.dt_txt), "h:mm a")}
                       </p>
-                      <WeatherIcon iconName={d.weather[0].icon} />
+                      <WeatherIcon iconName={getDayOrNightIcon(d.weather[0].icon, d.dt_txt)} />
                       <p>{convertKelvinToCelsius(d?.main.temp ?? 0)}°</p>
                     </div>
                   ))}
