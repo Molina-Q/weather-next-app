38b6da29e978205f9999a75b083b4e76ecd2b028
Molina-Q <quentinmolina1@gmail.com>
2024-04-01 11:06:28 +0200

    minor changes

--- src/utils/getDayOrNightIcon.ts
+++ src/utils/getDayOrNightIcon.ts
@@ -1,11 +1,13 @@
+/** @format */
+
 export function getDayOrNightIcon(
     iconName: string,
     dateTimeString: string
-): string {
-    const hours = new Date(dateTimeString).getHours();
-
-    const isDayTime = hours >= 6 && hours < 18;
-
+  ): string {
+    const hours = new Date(dateTimeString).getHours(); // Get hours from the given date and time string
+  
+    const isDayTime = hours >= 6 && hours < 18; // Consider daytime from 6 AM to 6 PM
+  
     return isDayTime ? iconName.replace(/.$/, "d") : iconName.replace(/.$/, "n");
-}
+  }
   
