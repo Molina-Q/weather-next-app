8034e16c32b4bb333aa1e6478fe9dad8f5c8b9b2
Molina-Q <quentinmolina1@gmail.com>
2024-04-07 14:58:29 +0200

    finished component

--- src/components/ForecastWeatherDetail.tsx
+++ src/components/ForecastWeatherDetail.tsx
@@ -1,10 +1,55 @@
 import React from 'react'
 import Container from './Container'
+import WeatherIcon from './WeatherIcon'
+import WeatherDetails, { WeatherDetailProps } from './WeatherDetails';
+import { convertKelvinToCelsius } from '@/utils/convertKelvinToCelsius';
 
 type Props = {}
 
-export default function ForecastWeatherDetail({}: Props) {
+export interface ForcastWeatherDetailProps extends WeatherDetailProps {
+  weatherIcon: string;
+  date: string;
+  day: string;
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temps_max: number;
+  description: string;
+}
+
+export default function ForecastWeatherDetail(props: ForcastWeatherDetailProps) {
+  const {
+    weatherIcon = "02d",
+    date = "19.09",
+    day = "Tuesday",
+    temp,
+    feels_like,
+    temp_min,
+    temps_max,
+    description,
+  } = props;
   return (
-    <Container>ForecastWeatherDetail</Container>
+    <Container className='gap-4'>
+      {/* left section */}
+      <section className='flex gap-4 items-center px-4'>
+        <div>
+          <WeatherIcon iconName={weatherIcon} />
+          <p>{date}</p>
+          <p className='text-sm'>{day}</p>
+        </div>
+
+        <div className='flex flex-col px-4'>
+          <span className='text-5xl' >{convertKelvinToCelsius(temp ?? 0)}°Z</span>
+          <p className='text-xs space-x-1 whitespace-nowrap'>
+            <span>Feels like</span>
+            <span>{convertKelvinToCelsius(feels_like ?? 0)}°</span>
+          </p>
+        </div>
+      </section>
+      {/* right section */}
+      <section className='overflow-c-auto flex justify-between gap-4 w-full pr-10'>
+        <WeatherDetails {...props} />
+      </section>
+    </Container>
   )
 }
