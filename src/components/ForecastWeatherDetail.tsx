ca97d0073b1124a7d3f5c306e67c8ef7efa961bc
Molina-Q <quentinmolina1@gmail.com>
2024-04-07 19:16:11 +0200

    minor changes

--- src/components/ForecastWeatherDetail.tsx
+++ src/components/ForecastWeatherDetail.tsx
@@ -13,7 +13,7 @@ export interface ForcastWeatherDetailProps extends WeatherDetailProps {
   temp: number;
   feels_like: number;
   temp_min: number;
-  temps_max: number;
+  temp_max: number;
   description: string;
 }
 
@@ -25,21 +25,21 @@ export default function ForecastWeatherDetail(props: ForcastWeatherDetailProps) 
     temp,
     feels_like,
     temp_min,
-    temps_max,
+    temp_max,
     description,
   } = props;
   return (
     <Container className='gap-4'>
       {/* left section */}
       <section className='flex gap-4 items-center px-4'>
-        <div>
-          <WeatherIcon iconName={weatherIcon} />
+        <div className='flex flex-col gap-1 items-center'>
+          <WeatherIcon iconName={weatherIcon}/>
           <p>{date}</p>
           <p className='text-sm'>{day}</p>
         </div>
 
         <div className='flex flex-col px-4'>
-          <span className='text-5xl' >{convertKelvinToCelsius(temp ?? 0)}°Z</span>
+          <span className='text-5xl' >{convertKelvinToCelsius(temp ?? 0)}°</span>
           <p className='text-xs space-x-1 whitespace-nowrap'>
             <span>Feels like</span>
             <span>{convertKelvinToCelsius(feels_like ?? 0)}°</span>
